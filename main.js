bf9f19670585f0a7402ad77328df62ea9cf0baaa
KilometersMiles <miles.l.higginson@gmail.com>
2026-06-23 10:42:00 -0600

    added logo

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, 'public/favicon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'), 
     },
